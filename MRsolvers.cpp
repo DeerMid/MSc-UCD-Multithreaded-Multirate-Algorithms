e5f70aa397d75d5e3a65a5826defef383dd5a288
Diarmuid Everard Coleman <diarmuid.everardcoleman@ucdconnect.ie>
2022-08-01 08:23:43 +0100

    further adding to MRsolvers.cpp, initial refinement identification complete

--- MRsolvers.cpp
+++ MRsolvers.cpp
@@ -11,9 +11,13 @@ void solRK12MR(vector<float>& t, vector<float>& y, float h, float T, float (*f)(
 	const float nu = 0.7;
 	const float hLo = 0.1;
 	const float hHi = 5;
+	const float deltaRK = 5e-1;
 	float fac = 1.0; 
-	float nErrMax = 0.0; //preinitialise
-	
+	float nErrMax = 1.0; //preinitialise
+	float nref_nErrMax = 0.0;
+
+	vector<float> nErrVec; //vector to store a time step's normalised error	
+
 	float t0 = t.back();
 
 	while (t0 < T) {
@@ -28,9 +32,52 @@ void solRK12MR(vector<float>& t, vector<float>& y, float h, float T, float (*f)(
 
 				float tolC = relTol * fabs(y[i + (dim * inter.count)]) + absTol;
 				float nErr = err / tolC;
-				if (nErrMax < nErr) nErrMax = nErr; //stores the largest error value
-
+				nErrVec[i] = nErr; //stores that node's normalised error
 			}
+
+			//check for the maximum error
+			for(int i = 0; i < dim; i++){
+				if(nErrMax <= nErrVec[i]) nErrMax = nErrVec[i];
+			}
+
+			vector<bool> ref (dim, false); //create a default refinement vector where no node needs refining
+			
+			//now we check for refinement
+			if(nErrMax > 1.0){ //if the worst error is outside tolerance then some refinement is necessary
+				for(int i = 0; i < dim; i++){
+					if(nErrVec[i] > (deltaRK*nErrMax)) ref[i] = false; //identifies nodes which need refinement
+				}
+			}
+
+			//we now need to identify the max error of the NOT to be refined set
+			for(int i = 0; i < dim; i++){
+				if(ref[i] == false){
+					if(nref_nErrMax < nErrVec[i]) nref_nErrMax = nErrVec[i];
+				}
+			}
+			
+			//this step determines how "large" the refinement will be, this can probably be improved much further in terms of efficiency
+			int check = 0;
+			for(int i = 0; i < dim; i++) check += ref[i];
+
+			if(check == dim){ //all components need to be refined
+				fac = nu * pow(nErrMax, -0.5);
+				fac = max(hLo, fac);
+				h = fac * h;
+			}
+			else if(nref_nErrMax > 1.0){
+				fac = nu * pow(nref_nErrMax, -0.5);
+				fac = max(hLo, fac);
+				h = fac * h;
+			}
+			else{
+				if(check > 0){ //this checks if there are components that need refinement
+					float Tin = t0 + h;
+				}
+			}
+
+			
+
 			//debugging tool, force closes the while loop and prints a message 
 			cout << "Made it this far!" << endl;
 			t0 = T + 100;
